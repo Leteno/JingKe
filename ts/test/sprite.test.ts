460190d98a77eb116fba0fc3afc6ef98bd7079f7
zhengjunhao <changchunjunhao@gmail.com>
2021-06-14 22:26:59 +0800

    Add Test on Sprite: isCollideWith

---
+++ ts/test/sprite.test.ts
@@ -0,0 +1,27 @@
+import Sprite from "../widgets/sprite"
+
+test("isCollideWith", () => {
+  let s1 = new Sprite();
+  let s2 = new Sprite();
+
+  s1.width = s1.height = s2.width = s2.height = 100;
+  s1.x = s1.y = 300;
+
+  // No touch
+  s2.x = s2.y = 0;
+  expect(s1.isCollideWith(s2)).toBe(false);
+  s2.x = 400; s2.y = 0;
+  expect(s1.isCollideWith(s2)).toBe(false);
+
+  // Border
+  s2.x = 200; s2.y = 300;
+  expect(s1.isCollideWith(s2)).toBe(false);
+  s2.x = 400; s2.y = 400;
+  expect(s1.isCollideWith(s2)).toBe(false);
+
+  // Cover
+  s2.x = 250; s2.y = 250;
+  expect(s1.isCollideWith(s2)).toBe(true);
+  s2.x = 350; s2.y = 350;
+  expect(s1.isCollideWith(s2)).toBe(true);
+})
