7f76841f147418c4bb0de6165540da4b84f66959
zhengjunhao <changchunjunhao@gmail.com>
2021-06-26 22:00:03 +0800

    Test clicking on child

--- ts/widgets/__test__/clickevent.test.ts
+++ ts/widgets/__test__/clickevent.test.ts
@@ -1,6 +1,8 @@
 
 import { ClickEvent } from "../../misc/event";
+import { Align, LayoutParams } from "../../misc/layout";
 import Panel from "../panel"
+import Sprite from "../sprite";
 import TestSprite from "./test_sprite.test"
 
 test("testSimpleView", () => {
@@ -50,5 +52,61 @@ test("testPanel", () => {
   expect(panel.onclick(event)).toBe(true);
   expect(viewAClick.mock.calls.length).toBe(1);
   expect(panelClick.mock.calls.length).toBe(1); // no change
+
+})
+
+test("testCenterChild", () => {
+  let panel = new Panel();
+  panel.forceWidth = panel.forceHeight = 400;
+  let panelOnClick = jest.fn(() => {
+    return true;
+  });
+  panel.onclickInternal = panelOnClick;
+  let s = new TestSprite();
+  s.layoutParam = new LayoutParams(Align.CENTER, Align.CENTER);
+  s.forceWidth = s.forceHeight = 100;
+  let spriteOnClick = jest.fn(() => {
+    return true;
+  });
+  s.onclickInternal = spriteOnClick;
+  panel.addView(s);
+
+  panel.measure({} as CanvasRenderingContext2D)
+  panel.layout();
+
+  // Click on panel
+  panel.onclick(new ClickEvent(0, 0));
+  expect(panelOnClick.mock.calls.length).toBe(1);
+  expect(spriteOnClick.mock.calls.length).toBe(0);
+
+  // Click on left-top of sprite.
+  panel.onclick(new ClickEvent(150, 150));
+  expect(panelOnClick.mock.calls.length).toBe(1);
+  expect(spriteOnClick.mock.calls.length).toBe(1);
+
+  // Click on right-top of sprite.
+  panel.onclick(new ClickEvent(250, 150));
+  expect(panelOnClick.mock.calls.length).toBe(1);
+  expect(spriteOnClick.mock.calls.length).toBe(2);
+
+  // Click on left-bottom of sprite.
+  panel.onclick(new ClickEvent(150, 250));
+  expect(panelOnClick.mock.calls.length).toBe(1);
+  expect(spriteOnClick.mock.calls.length).toBe(3);
+
+  // Click on right-bottom of sprite.
+  panel.onclick(new ClickEvent(250, 250));
+  expect(panelOnClick.mock.calls.length).toBe(1);
+  expect(spriteOnClick.mock.calls.length).toBe(4);
+
+  // Click on center of sprite.
+  panel.onclick(new ClickEvent(200, 200));
+  expect(panelOnClick.mock.calls.length).toBe(1);
+  expect(spriteOnClick.mock.calls.length).toBe(5);
+
+  // Click on right-bottom of panel.
+  panel.onclick(new ClickEvent(400, 400));
+  expect(panelOnClick.mock.calls.length).toBe(2);
+  expect(spriteOnClick.mock.calls.length).toBe(5);
 
 })
