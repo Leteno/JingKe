08d544b31be79c1d110bfafc269407c31761e644
zhengjunhao <changchunjunhao@gmail.com>
2021-07-11 17:37:28 +0800

    Ignore auto generated js in js/

--- js/misc/time.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.timestamp = void 0;
-function timestamp() {
-    return window.performance && window.performance.now ?
-        window.performance.now() * 1000 :
-        new Date().getTime();
-}
-exports.timestamp = timestamp;
