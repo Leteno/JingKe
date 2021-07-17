df4d54008fa8ba8b04902f496aa67d18b1925306
zhengjunhao <changchunjunhao@gmail.com>
2021-07-18 00:03:37 +0800

    Add Character.isAlphanumberic

---
+++ ts/misc/character.ts
@@ -0,0 +1,8 @@
+
+export class Character {
+  static isAlphanumberic(ch: number) {
+    return (ch >= 48 && ch <= 57) || // 0-9
+    (ch >= 65 && ch <= 90) || // A-Z
+    (ch >= 97 && ch <= 122);
+  }
+}
