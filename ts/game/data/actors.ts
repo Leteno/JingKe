7156f4eac43f561313a40d4b5d07cf8b531f2c7b
zhengjunhao <changchunjunhao@gmail.com>
2021-08-11 21:36:42 +0800

    Orgnize all 特技 into specials.ts

--- ts/game/data/actors.ts
+++ ts/game/data/actors.ts
@@ -1,5 +1,6 @@
 import { People } from "../../compose/place_and_people_view";
 import { ABILITY, Special } from "../../data/character";
+import { Specials } from "../../data/specials";
 
 export class Actors {
   businessman: People;
@@ -11,22 +12,28 @@ export class Actors {
     fanwuji.character.name = "樊于期";
     fanwuji.character.imageSrc = "res/copyleft/people_fanwuji.png";
     fanwuji.character.abilities[ABILITY.ATTACK] = 10;
-    fanwuji.character.specials.push(new Special("勇武", "遇到攻击力比自己高的，攻击力+3"));
+    fanwuji.character.specials.push(
+      Specials.getInstance().brave
+    );
     this.fanwuji = fanwuji;
 
     let juzi = new People();
     juzi.character.name = "莫翟";
     juzi.character.imageSrc = "res/copyleft/people_juzi.png";
     juzi.character.abilities[ABILITY.LOYAL] = 10;
-    juzi.character.specials.push(new Special("闲庭", "由于你的步伐过于冷静，正常情况下不会遭遇捕快"));
-    juzi.character.specials.push(new Special("义薄云天", "别人遇到任何困难，你都会伸出援手"));
+    juzi.character.specials.push(
+      Specials.getInstance().xianting,
+      Specials.getInstance().yiboyuntian
+    );
     this.juzi = juzi;
 
     let businessman = new People();
     businessman.character.name = "商人";
     businessman.character.imageSrc = "res/copyleft/people_businessman.png";
     businessman.character.abilities[ABILITY.INTELIGENCE] = 10;
-    businessman.character.specials.push(new Special("口若悬河", "商店购买时，要价便宜 30%"));
+    businessman.character.specials.push(
+      Specials.getInstance().kouruoxuanhe
+    );
     this.businessman = businessman;
   }
 }
