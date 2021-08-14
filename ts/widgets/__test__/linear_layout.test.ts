7e201a64c2215ead23ea1931f2c4c034ae0d0bd1
zhengjunhao <changchunjunhao@gmail.com>
2021-08-14 16:42:55 +0800

    LinearLayout support Landscape

--- ts/widgets/__test__/linear_layout.test.ts
+++ ts/widgets/__test__/linear_layout.test.ts
@@ -1,6 +1,8 @@
 
 import LinearLayout from "../linear_layout"
+import {Orientation} from "../linear_layout"
 import TestSprite from "./test_sprite.test"
+
 test("measure", () => {
   let layout = new LinearLayout();
 
@@ -17,8 +19,13 @@ test("measure", () => {
   layout.addView(v2);
 
   let ctx = {} as CanvasRenderingContext2D;
-  layout.measure(ctx, 600, 600);
+  let measureResult = layout.measure(ctx, 600, 600);
   layout.layout(600, 600);
+
+  expect(measureResult.calcWidth)
+    .toBe(205);
+  expect(measureResult.calcHeight)
+    .toBe(400);
 
   expect(v1.width).toBe(100);
   expect(v1.height).toBe(100);
@@ -29,4 +36,42 @@ test("measure", () => {
   expect(v2.height).toBe(200);
   expect(v2.x).toBe(5);
   expect(v2.y).toBe(180);
+})
+
+test("measure horizontal", () => {
+  let layout = new LinearLayout();
+  layout.orientation = Orientation.HORIZONTAL;
+
+  let v1 = new TestSprite(100, 100);
+  v1.margin.top = 30;
+  v1.margin.bottom = 40;
+  v1.margin.left = 10;
+  v1.margin.right = 33;
+  layout.addView(v1);
+
+  let v2 = new TestSprite(200, 200);
+  v2.margin.top = 10;
+  v2.margin.bottom = 20;
+  v2.margin.left = 5;
+  v2.margin.right = 17;
+  layout.addView(v2);
+
+  let ctx = {} as CanvasRenderingContext2D;
+  let measureResult = layout.measure(ctx, 600, 600);
+  layout.layout(600, 600);
+
+  expect(measureResult.calcHeight)
+    .toBe(230);
+  expect(measureResult.calcWidth)
+    .toBe(365);
+
+  expect(v1.width).toBe(100);
+  expect(v1.height).toBe(100);
+  expect(v1.x).toBe(10);
+  expect(v1.y).toBe(30);
+
+  expect(v2.width).toBe(200);
+  expect(v2.height).toBe(200);
+  expect(v2.x).toBe(148);
+  expect(v2.y).toBe(10);
 })
--- ts/widgets/linear_layout.ts
+++ ts/widgets/linear_layout.ts
@@ -1,35 +1,99 @@
 import Panel from "./panel";
 import { MeasureResult } from "./sprite";
+export enum Orientation {
+  HORIZONTAL,
+  VERTICAL
+}
 
 export default class LinearLayout extends Panel {
+  orientation: Orientation;
+
+  constructor() {
+    super();
+    this.orientation = Orientation.VERTICAL;
+  }
+
   // Child views are layout potrait.
   calculateActualSize(
     ctx: CanvasRenderingContext2D,
     maxWidthForCalculation: number,
     maxHeightForCalculation: number): MeasureResult {
-    let childWidthAtMost = 0;
-    let currentHeight = 0;
-
-    this.children.forEach(view => {
-      let size = view.measure(
+    if (this.orientation == Orientation.VERTICAL) {
+      return this.measureVertical(
         ctx,
         maxWidthForCalculation,
         maxHeightForCalculation
       );
-      childWidthAtMost = Math.max(
-        size.calcWidth,
-        childWidthAtMost
-      );
-      currentHeight += size.calcHeight;
-    });
-
-    return {
-      calcWidth: childWidthAtMost,
-      calcHeight: currentHeight,
+    } else {
+      return this.measureHorizontal(
+        ctx,
+        maxWidthForCalculation,
+        maxHeightForCalculation
+      )
     }
   }
 
+  measureVertical(
+    ctx: CanvasRenderingContext2D,
+    maxWidthForCalculation: number,
+    maxHeightForCalculation: number) {
+      let childWidthAtMost = 0;
+      let currentHeight = 0;
+  
+      this.children.forEach(view => {
+        let size = view.measure(
+          ctx,
+          maxWidthForCalculation,
+          maxHeightForCalculation
+        );
+        childWidthAtMost = Math.max(
+          size.calcWidth,
+          childWidthAtMost
+        );
+        currentHeight += size.calcHeight;
+      });
+  
+      return {
+        calcWidth: childWidthAtMost,
+        calcHeight: currentHeight,
+      }
+  }
+
+  measureHorizontal(
+    ctx: CanvasRenderingContext2D,
+    maxWidthForCalculation: number,
+    maxHeightForCalculation: number) {
+      let childHeightAtMost = 0;
+      let currentWidth = 0;
+  
+      this.children.forEach(view => {
+        let size = view.measure(
+          ctx,
+          maxWidthForCalculation,
+          maxHeightForCalculation
+        );
+        childHeightAtMost = Math.max(
+          size.calcHeight,
+          childHeightAtMost
+        );
+        currentWidth += size.calcWidth;
+      });
+  
+      return {
+        calcWidth: currentWidth,
+        calcHeight: childHeightAtMost,
+      }
+  }
+
   onLayout(parentWidth: number, parentHeight: number) {
+    if (this.orientation == Orientation.VERTICAL) {
+      this.layoutVertical(parentWidth, parentHeight);
+    } else {
+      this.layoutHorizontal(parentWidth, parentHeight);
+    }
+  }
+
+  layoutVertical(parentWidth: number, parentHeight: number) {
     let left = 0, top = 0;
     this.children.forEach(view => {
       view.layout(
@@ -39,6 +103,19 @@ export default class LinearLayout extends Panel {
         top
       )
       top += view.margin.top + view.height + view.margin.bottom;
+    });
+  }
+  
+  layoutHorizontal(parentWidth: number, parentHeight: number) {
+    let left = 0, top = 0;
+    this.children.forEach(view => {
+      view.layout(
+        this.width - this.padding.left - this.padding.right,
+        this.height - this.padding.top - this.padding.bottom,
+        left,
+        top
+      )
+      left += view.margin.left + view.width + view.margin.right;
     });
   }
 }
