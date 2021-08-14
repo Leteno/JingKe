78f6fe661a726be523db9dd52d81b784717b8454
zhengjunhao <changchunjunhao@gmail.com>
2021-08-14 16:13:15 +0800

    Fix test fail due to theme change..

--- ts/animator/__test__/text-affect.test.ts
+++ ts/animator/__test__/text-affect.test.ts
@@ -4,26 +4,26 @@ import {textAlpha} from "../text-affect"
 
 test("textAlphaFadeIn", () => {
   let textView = {
-    textColor: "#FFFFFF"
-  } as TextView;
-
-  let fadeIn = textAlpha(true, 2000, textView);
-  expect(textView.textColor).toBe("#FFFFFF")
-  fadeIn.update(1000);
-  expect(textView.textColor).toBe("#7f7f7f")
-  fadeIn.update(1000);
-  expect(textView.textColor).toBe("#000000")
-})
-
-test("textAlphaFadeOut", () => {
-  let textView = {
     textColor: "#000000"
   } as TextView;
 
-  let fadeIn = textAlpha(false, 2000, textView);
+  let fadeIn = textAlpha(true, 2000, textView);
   expect(textView.textColor).toBe("#000000")
   fadeIn.update(1000);
   expect(textView.textColor).toBe("#7f7f7f")
   fadeIn.update(1000);
   expect(textView.textColor).toBe("#ffffff")
+})
+
+test("textAlphaFadeOut", () => {
+  let textView = {
+    textColor: "#ffffff"
+  } as TextView;
+
+  let fadeIn = textAlpha(false, 2000, textView);
+  expect(textView.textColor).toBe("#ffffff")
+  fadeIn.update(1000);
+  expect(textView.textColor).toBe("#7f7f7f")
+  fadeIn.update(1000);
+  expect(textView.textColor).toBe("#000000")
 })
