9fd8f2f49133f7c22f7032fd9b8fffe4b32fb7cb
zhengjunhao <changchunjunhao@gmail.com>
2021-08-15 14:37:42 +0800

    Add PageList to support page view

---
+++ ts/compose/__test__/page_list.test.ts
@@ -0,0 +1,107 @@
+
+import TextView, { Text } from "../../widgets/textview";
+import {PageList} from "../page_list"
+import {defaultCtx} from "../../widgets/__test__/default_value.test"
+import { ClickEvent } from "../../misc/event";
+
+test("common", () => {
+  let pageList = new PageList();
+
+  let tx1 = new TextView(new Text("A"));
+  let tx2 = new TextView(new Text("B2"));
+  pageList.addPage("tx1", tx1);
+  pageList.addPage("tx2", tx2);
+
+  pageList.measure(defaultCtx, 500, 500);
+  pageList.layout(500, 500);
+  pageList.drawToCanvas(defaultCtx);
+
+  expect(pageList.titlePanel.children.length)
+    .toBe(2)
+  expect((pageList.titlePanel.children[0] as TextView)
+    .text.content).toBe("tx1");
+  expect((pageList.titlePanel.children[1] as TextView)
+    .text.content).toBe("tx2");
+  expect(pageList.listInfo.currentIndex)
+    .toBe(0);
+  expect(pageList.contentPanel.children.length)
+    .toBe(1);
+  expect(pageList.contentPanel.children[0])
+    .toBe(tx1);
+  expect((pageList.titlePanel.children[0] as TextView)
+    .textColor).toBe(pageList.listInfo.textSelectedColor)
+  expect((pageList.titlePanel.children[1] as TextView)
+    .textColor).toBe(pageList.listInfo.textUnselectedColor)
+
+  // Test data binding
+  pageList.listInfo.pages[0].title = "tx0";
+  pageList.listInfo.currentIndex = 1;
+  pageList.listInfo.dirty = true;
+
+  pageList.drawToCanvas(defaultCtx);
+
+  // Changes on TitlePanel
+  expect((pageList.titlePanel.children[0] as TextView)
+    .text.content).toBe("tx0");
+  expect((pageList.titlePanel.children[0] as TextView)
+    .textColor).toBe(pageList.listInfo.textUnselectedColor)
+  expect((pageList.titlePanel.children[1] as TextView)
+    .textColor).toBe(pageList.listInfo.textSelectedColor)
+
+  // Changes on ContentPanel
+  expect(pageList.contentPanel.children.length)
+  .toBe(1);
+  expect(pageList.contentPanel.children[0])
+    .toBe(tx2);
+})
+
+test("click on Title, page will switch", () => {
+  let pageList = new PageList();
+
+  let tx1 = new TextView(new Text("A"));
+  let tx2 = new TextView(new Text("B2"));
+  pageList.addPage("tx1", tx1);
+  pageList.addPage("tx2", tx2);
+
+  pageList.measure(defaultCtx, 500, 500);
+  pageList.layout(500, 500);
+  pageList.drawToCanvas(defaultCtx);
+
+  expect(pageList.listInfo.currentIndex)
+    .toBe(0);
+  expect((pageList.titlePanel.children[0] as TextView)
+    .textColor).toBe(pageList.listInfo.textSelectedColor)
+  expect((pageList.titlePanel.children[1] as TextView)
+    .textColor).toBe(pageList.listInfo.textUnselectedColor)
+  expect(pageList.contentPanel.children[0])
+    .toBe(tx1);
+
+  // Perform clicking on tx2
+  pageList.titlePanel.children[1]
+    .onclickInternal(new ClickEvent(0, 0));
+  pageList.drawToCanvas(defaultCtx);
+
+  // The page should switch to page2
+  expect(pageList.listInfo.currentIndex)
+    .toBe(1);
+  expect((pageList.titlePanel.children[0] as TextView)
+    .textColor).toBe(pageList.listInfo.textUnselectedColor)
+  expect((pageList.titlePanel.children[1] as TextView)
+    .textColor).toBe(pageList.listInfo.textSelectedColor)
+  expect(pageList.contentPanel.children[0])
+    .toBe(tx2);
+
+  // Perform clicking on tx1
+  pageList.titlePanel.children[0]
+    .onclickInternal(new ClickEvent(0, 0));
+  pageList.drawToCanvas(defaultCtx);
+  // The page should switch back to page1
+  expect(pageList.listInfo.currentIndex)
+    .toBe(0);
+  expect((pageList.titlePanel.children[0] as TextView)
+    .textColor).toBe(pageList.listInfo.textSelectedColor)
+  expect((pageList.titlePanel.children[1] as TextView)
+    .textColor).toBe(pageList.listInfo.textUnselectedColor)
+  expect(pageList.contentPanel.children[0])
+    .toBe(tx1);
+})
---
+++ ts/compose/page_list.ts
@@ -0,0 +1,101 @@
+import { BindableData } from "../data/bindable_data";
+import LinearLayout, { Orientation } from "../widgets/linear_layout";
+import Panel from "../widgets/panel";
+import Sprite from "../widgets/sprite";
+import TextView, { Text } from "../widgets/textview";
+
+export class PageInfo {
+  title: string;
+  page: Sprite;
+}
+
+export class PageListInfo extends BindableData {
+  currentIndex: number;
+  textSelectedColor: string;
+  textUnselectedColor: string;
+
+  pages: Array<PageInfo>;
+
+  constructor() {
+    super();
+    this.currentIndex = -1;
+    this.pages = new Array<PageInfo>();
+  }
+}
+
+export class PageList extends LinearLayout {
+  listInfo: PageListInfo;
+
+  // views
+  titlePanel: LinearLayout;
+  contentPanel: Panel;
+
+  constructor() {
+    super();
+    this.padding.left = this.padding.right
+      = this.padding.top = this.padding.bottom
+      = 20;
+    this.titlePanel = new LinearLayout();
+    this.titlePanel.orientation = Orientation.HORIZONTAL;
+    this.titlePanel.margin.bottom = 20;
+    this.contentPanel = new Panel();
+    this.addView(this.titlePanel);
+    this.addView(this.contentPanel);
+
+    this.listInfo = new PageListInfo();
+    this.listInfo.textSelectedColor = "#000000";
+    this.listInfo.textUnselectedColor = "#5c5c5c";
+    this.bindData(
+      this.listInfo,
+      PageList.onListInfoChange.bind(this)
+    );
+
+    this.bgColor = "#f0f0f0";
+  }
+
+  addPage(title: string, page: Sprite) {
+    let info = new PageInfo();
+    info.title = title;
+    info.page = page;
+    this.listInfo.pages.push(info);
+    this.listInfo.dirty = true;
+  }
+
+  private static onListInfoChange(pageList: PageList, listInfo: PageListInfo) {
+    if (listInfo.currentIndex < 0) {
+      listInfo.currentIndex = 0;
+    }
+    if (listInfo.pages.length <= 0) {
+      return;
+    }
+    if (listInfo.currentIndex > listInfo.pages.length) {
+      listInfo.currentIndex = listInfo.pages.length - 1;
+    }
+
+    // title layer
+    pageList.titlePanel.removeAllViews();
+    for (let i = 0; i < listInfo.pages.length; i++) {
+      let info = listInfo.pages[i];
+      let title = new TextView(new Text(info.title));
+      title.onclickInternal = (ignore) => {
+        listInfo.currentIndex = i;
+        listInfo.dirty = true;
+        return true;
+      }
+      if (i == listInfo.currentIndex) {
+        title.textColor = listInfo.textSelectedColor;
+      } else {
+        title.textColor = listInfo.textUnselectedColor;
+      }
+      pageList.titlePanel.addView(title);
+    }
+    pageList.titlePanel.setIsDirty(true);
+
+    // content layer
+    let currentPage: Sprite =
+      pageList.listInfo.pages[listInfo.currentIndex].page;
+    pageList.contentPanel.removeAllViews();
+    pageList.contentPanel.addView(currentPage);
+    pageList.contentPanel.setIsDirty(true);
+  }
+}
