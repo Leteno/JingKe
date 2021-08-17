b8227d039cc2b124fe247ac15f6ef63ee7fc12e3
zhengjunhao <changchunjunhao@gmail.com>
2021-08-17 23:02:55 +0800

    ScrollView support scrollBy(x,y)

---
+++ ts/widgets/__test__/scrollview.test.ts
@@ -0,0 +1,48 @@
+import {ScrollView} from "../scrollview"
+
+test("scrollBy", () => {
+  let scrollView = new ScrollView();
+  scrollView.width = 100;
+  scrollView.height = 200;
+  scrollView.childrenMaxWidth = 200;
+  scrollView.childrenMaxHeight = 400;
+
+  expect(scrollView.offsetX).toBe(0);
+  expect(scrollView.offsetY).toBe(0);
+
+  scrollView.scrollBy(10, 10);
+  expect(scrollView.offsetX).toBe(10);
+  expect(scrollView.offsetY).toBe(10);
+
+  scrollView.scrollBy(-10, -40);
+  expect(scrollView.offsetX).toBe(0);
+  expect(scrollView.offsetY).toBe(0);
+
+  scrollView.scrollBy(200, 200);
+  expect(scrollView.offsetX).toBe(100);
+  expect(scrollView.offsetY).toBe(200);
+})
+
+test("scrollBy big parent small children", () => {
+
+  let scrollView = new ScrollView();
+  scrollView.width = 200;
+  scrollView.height = 400;
+  scrollView.childrenMaxWidth = 100;
+  scrollView.childrenMaxHeight = 100;
+
+  expect(scrollView.offsetX).toBe(0);
+  expect(scrollView.offsetY).toBe(0);
+
+  scrollView.scrollBy(10, 10);
+  expect(scrollView.offsetX).toBe(0);
+  expect(scrollView.offsetY).toBe(0);
+
+  scrollView.scrollBy(-10, -40);
+  expect(scrollView.offsetX).toBe(0);
+  expect(scrollView.offsetY).toBe(0);
+
+  scrollView.scrollBy(200, 200);
+  expect(scrollView.offsetX).toBe(0);
+  expect(scrollView.offsetY).toBe(0);
+})
--- ts/widgets/scrollview.ts
+++ ts/widgets/scrollview.ts
@@ -1,4 +1,5 @@
 import Panel from "./panel";
+import { MeasureResult } from "./sprite";
 
 export class ScrollView extends Panel {
   // vertical
@@ -6,10 +7,18 @@ export class ScrollView extends Panel {
   offsetX: number;
   offsetY: number;
 
+  scrollOffset: number;
+  childrenMaxWidth: number;
+  childrenMaxHeight: number;
+
   constructor() {
     super();
     this.offsetX = 0;
     this.offsetY = 0;
+
+    this.scrollOffset = 10;
+    this.childrenMaxWidth = 0;
+    this.childrenMaxHeight = 0;
   }
 
   drawToCanvasInternal(ctx: CanvasRenderingContext2D) {
@@ -20,5 +29,43 @@ export class ScrollView extends Panel {
     ctx.translate(this.offsetX, this.offsetY);
     super.drawToCanvasInternal(ctx);
     ctx.restore();
+  }
+
+  calculateActualSize(
+    ctx: CanvasRenderingContext2D,
+    maxWidthForCalculation: number,
+    maxHeightForCalculation: number): MeasureResult {
+    let result = super.calculateActualSize(
+      ctx, maxWidthForCalculation, maxHeightForCalculation
+    );
+    this.childrenMaxWidth = result.calcWidth;
+    this.childrenMaxHeight = result.calcHeight;
+    return result;
+  }
+    
+  scrollBy(x: number, y: number) {
+    let newOffsetX = x + this.offsetX;
+    if (newOffsetX < 0) {
+      newOffsetX = 0;
+    } else if (newOffsetX + this.width > this.childrenMaxWidth) {
+      if (this.width < this.childrenMaxWidth) {
+        newOffsetX = this.childrenMaxWidth - this.width;
+      } else {
+        newOffsetX = 0;
+      }
+    }
+    this.offsetX = newOffsetX;
+
+    let newOffsetY = y + this.offsetY;
+    if (newOffsetY < 0) {
+      newOffsetY = 0;
+    } else if (newOffsetY + this.height > this.childrenMaxHeight) {
+      if (this.height < this.childrenMaxHeight) {
+        newOffsetY = this.childrenMaxHeight - this.height;
+      } else {
+        newOffsetY = 0;
+      }
+    }
+    this.offsetY = newOffsetY;
   }
 }
