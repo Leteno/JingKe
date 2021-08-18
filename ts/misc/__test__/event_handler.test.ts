a71cd86f1e220d4e7712517c6409afa0aca76f32
zhengjunhao <changchunjunhao@gmail.com>
2021-08-18 22:04:26 +0800

    Add test on event_handler

--- ts/misc/__test__/event_handler.test.ts
+++ ts/misc/__test__/event_handler.test.ts
@@ -1,25 +1,64 @@
 
+import { waitForMs } from "../concurency";
 import EventHandler from "../event_handler"
-
-function MockPointDownEvent(
-  x: number, y: number) {
-  let point = new PointerEvent("pointerdown");
-  point.initMouseEvent("onpointerdown",
-    true, true, window, 0,
-    x, y, 0, 0,
-    false, false, false,false, 0,null
-  );
-  return point;
-}
 
 test("click", () => {
   let eventHandler = new EventHandler();
   let onClick = jest.fn();
   eventHandler.bindOnClickHandler(onClick);
-  let point = MockPointDownEvent(30, 30);
-  eventHandler.onpointerdown(point);
-  eventHandler.onpointerup(point);
+  eventHandler.onpointerdown(30, 30);
+  eventHandler.onpointerup(30, 30);
 
   expect(onClick.mock.calls.length)
     .toBe(1);
+})
+
+test("press", async () => {
+  let eventHandler = new EventHandler();
+  let onClick = jest.fn();
+  let onPress = jest.fn();
+  eventHandler.bindOnClickHandler(onClick);
+  eventHandler.bindOnPressHandler(onPress);
+
+  eventHandler.onpointerdown(30, 30);
+
+  await waitForMs(2000);
+  expect(onPress.mock.calls.length)
+    .toBe(1);
+  expect(onClick.mock.calls.length)
+    .toBe(0);
+})
+
+test("click not canceled by small move", async() => {
+  let eventHandler = new EventHandler();
+  let onClick = jest.fn();
+  let onPress = jest.fn();
+  eventHandler.bindOnClickHandler(onClick);
+  eventHandler.bindOnPressHandler(onPress);
+
+  eventHandler.onpointerdown(30, 30);
+  eventHandler.onpointermove(35, 35);
+  eventHandler.onpointerup(35, 35);
+
+  expect(onPress.mock.calls.length)
+    .toBe(0);
+  expect(onClick.mock.calls.length)
+    .toBe(1);
+})
+
+test("click canceled by big move", async() => {
+  let eventHandler = new EventHandler();
+  let onClick = jest.fn();
+  let onPress = jest.fn();
+  eventHandler.bindOnClickHandler(onClick);
+  eventHandler.bindOnPressHandler(onPress);
+
+  eventHandler.onpointerdown(30, 30);
+  eventHandler.onpointermove(50, 50);
+  eventHandler.onpointerup(50, 50);
+
+  expect(onPress.mock.calls.length)
+    .toBe(0);
+  expect(onClick.mock.calls.length)
+    .toBe(0);
 })
