0f5a25ad1b48f1b5720e0166a471503491f22feb
zhengjunhao <changchunjunhao@gmail.com>
2021-09-02 23:26:20 +0800

    Introduce game_state to skip some state

---
+++ ts/game/__test__/game_state.test.ts
@@ -0,0 +1,35 @@
+
+import {GameState} from "../game_state"
+
+test("state check", () => {
+  let state = GameState.create();
+  state.recordState("hello");
+  state.recordState("world");
+  expect(state.hasEnterState("hello")).toBe(true);
+  expect(state.hasEnterState("world")).toBe(true);
+})
+
+test("switch logic", () => {
+  let state = GameState.create();
+  state.recordState("hello");
+  expect(state.hasEnterState("hello")).toBe(true);
+
+  state.switchToNewScene("new Scene");
+  expect(state.currentSceneName).toBe("new Scene");
+  expect(state.hasEnterState("hello")).toBe(false);
+})
+
+test("parcel test", () => {
+  let state = GameState.create();
+  state.currentSceneName = "abc";
+  state.recordState("hello");
+  state.recordState("world");
+
+  let p = state.toParcel();
+  let state2 = GameState.create();
+  state2.fromParcel(p);
+
+  expect(state2.currentSceneName).toBe("abc");
+  expect(state2.hasEnterState("hello")).toBe(true);
+  expect(state2.hasEnterState("world")).toBe(true);
+})
---
+++ ts/game/game_state.ts
@@ -0,0 +1,39 @@
+import Parcel from "../objects/parcel";
+import parcel from "../objects/parcel";
+import { Serializable } from "../objects/serializable";
+
+export class GameState implements Serializable {
+  currentSceneName: string;
+  // A simple way to add tag on states
+  private states: Array<string>;
+
+  static instance: GameState = new GameState();
+  private constructor() {
+    this.states = new Array<string>();
+  }
+
+  static create() {
+    return new GameState();
+  }
+
+  switchToNewScene(sceneName: string) {
+    this.currentSceneName = sceneName;
+    this.states = new Array<string>();
+  }
+  hasEnterState(state: string) {
+    return this.states.findIndex(item => item == state) >= 0;
+  }
+  recordState(state: string) {
+    this.states.push(state);
+  }
+  toParcel(): parcel {
+    let p = new Parcel();
+    p.writeString(this.currentSceneName);
+    p.writeStringArray(this.states);
+    return p;
+  }
+  fromParcel(p: parcel) {
+    this.currentSceneName = p.readString();
+    this.states = p.readStringArray();
+  }
+}
