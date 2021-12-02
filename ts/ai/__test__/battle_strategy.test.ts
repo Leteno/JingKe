b1d6bb6b608c093e2ee8f2e43ef63ff19071dbe2
Leteno <changchunjunhao@gmail.com>
2021-12-02 21:41:53 +0800

    Add ai battle_strategy: greedy

---
+++ ts/ai/__test__/battle_strategy.test.ts
@@ -0,0 +1,36 @@
+
+import { ABILITY, Character } from "../../data/character";
+import {Greedy} from "../batte_strategy"
+
+function createCharacter(
+    attack: number, defend: number,
+    agile: number, point: number): Character {
+    let ret = new Character();
+    ret.abilities[ABILITY.ATTACK] = attack;
+    ret.abilities[ABILITY.DEFEND] = defend;
+    ret.abilities[ABILITY.AGILE] = agile;
+    ret.abilities[ABILITY.POINT] = point;
+    return ret;
+}
+
+test("testGreedy", () => {
+    let strategy = new Greedy();
+
+    // possible attack << oppo.defend
+    let ch1 = createCharacter(2, 2, 2, 2);
+    let ch2 = createCharacter(2, 3, 2, 2);
+    let ret = strategy.fight(ch1, ch2);
+    expect(ret[ABILITY.ATTACK]).toBe(undefined);
+    expect(ret[ABILITY.DEFEND]).toBe(2);
+    expect(ret[ABILITY.AGILE]).toBe(0);
+
+    // attack >> oppo.defend
+    // add as-less-as-possible attack
+    // the rest would be agile
+    ch1 = createCharacter(2, 2, 2, 3);
+    ch2 = createCharacter(2, 2, 2, 1);
+    ret = strategy.fight(ch1, ch2);
+    expect(ret[ABILITY.ATTACK]).toBe(2);
+    expect(ret[ABILITY.DEFEND]).toBe(undefined);
+    expect(ret[ABILITY.AGILE]).toBe(1);
+})
---
+++ ts/ai/batte_strategy.ts
@@ -0,0 +1,27 @@
+import { ABILITY, Character } from "../data/character";
+
+
+export interface BattleStrategy {
+    fight(me: Character, oppo: Character): Array<number>;
+}
+export class Greedy implements BattleStrategy {
+    fight(me: Character, oppo: Character): Array<number> {
+        let result = [];
+        let leftPoint = me.abilities[ABILITY.POINT];
+        if (me.abilities[ABILITY.ATTACK] <= oppo.abilities[ABILITY.DEFEND]) {
+            let maxGap = oppo.abilities[ABILITY.DEFEND] + oppo.abilities[ABILITY.POINT]
+                - me.abilities[ABILITY.ATTACK] + 1;
+            if (maxGap >= leftPoint) {
+                // If we cannot harm oppo, we should defend ourselves
+                result[ABILITY.DEFEND] = leftPoint;
+                leftPoint = 0;
+            } else {
+                // Add the min attack that we need
+                result[ABILITY.ATTACK] = maxGap;
+                leftPoint -= maxGap;
+            }
+        }
+        result[ABILITY.AGILE] = leftPoint;
+        return result;
+    }
+}
