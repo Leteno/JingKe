e99f4bf91667ddb9fd3b119793e3ad3eef99c0fb
Leteno <changchunjunhao@gmail.com>
2022-04-26 17:41:10 +0800

    Add tsec check

---
+++ install.sh
@@ -0,0 +1,9 @@
+
+npm install
+npm install -g typescript
+
+# yarn
+npm i -g corepack
+
+# install tsec
+yarn add tsec --dev
--- publish.sh
+++ publish.sh
@@ -4,4 +4,8 @@ set -eux
 
 npx jest
 tsc
+
+# run tsec
+yarn tsec -p .\tsconfig.json
+
 ./node_modules/.bin/esbuild game.js --bundle --outfile=game.bundle.js
